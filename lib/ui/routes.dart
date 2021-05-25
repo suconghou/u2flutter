12b79247712c62b5ff0cbc44da49ea6afe95cca5
suconghou <1126045770@qq.com>
2021-05-25 23:02:49 +0800

    fix: save

--- lib/ui/routes.dart
+++ lib/ui/routes.dart
@@ -4,14 +4,14 @@ import 'pages/FavoritesPage.dart';
 import 'pages/PlayPage.dart';
 import 'pages/SettingsPage.dart';
 import 'pages/AboutPage.dart';
-import 'pages/DownloadPage.dart';
+import 'pages/SharePage.dart';
 import 'pages/ChannelPage.dart';
 import 'pages/CListVideos.dart';
 
 final Map<String, WidgetBuilder> routes = {
   "/": (c) => MainPage(),
   '/favorites': (c) => FavoritesPage(),
-  '/download': (c) => DownloadPage(),
+  '/share': (c) => SharePage(),
   '/channel':(c)=>ChannelPage(),
   '/listvideos':(c)=>CListVideos(),
   '/play': (c) => PlayPage(),
